3342a6474db21e57660fe4003ce5856d9914beaa
Viviane Bresolin <vivianebresolin@gmail.com>
2021-03-16 20:42:02 -0300

    Initial commit: project created with Spring Tool Suite

---
+++ src/main/java/com/vivianeBresolin/webservicespringjpa/WebserviceSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.vivianeBresolin.webservicespringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivianeBresolin/webservicespringjpa/WebserviceSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivianeBresolin.webservicespringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
